00efcc0297378fdfb479a2405cf03fac6f216500
Robert L. Schuhmann <robert.schuhmann.13@ucl.ac.uk>
2019-11-13 18:06:26 +0000

    unit test for encoding wrapper

--- test_preprocess_encode.py
+++ test_preprocess_encode.py
@@ -4,17 +4,19 @@ import pytest
 
 from preprocess_data import preprocessing
 from vgg16 import VGG16
+from encode_image import model_gen,encodings
 
 import os
 import numpy as np
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input
 
+
 captions_folder='Flickr8K_Text/'
 image_folder='Flickr8K_Data/'
 test_image='3666056567_661e25f54c.jpg'
 vgg16_pred_slice=np.array([0,0,0,0,6.281284,0,0,0,0,0.3895438,0,0,1.1056916,0,0,1.7175925,0,1.8917649,0,0,1.7590455,6.602196,0.43233678,0.4528695,0,3.6373415,3.5424447,2.4427955,0,0,3.206093,2.5562162,0,0,2.2278,0,3.2318673,2.3519747,0,6.4740357,0,2.8249745,0,0,9.216705,0,0,0,2.835004,2.1258173,2.2908218,1.0856905,2.6308882,0.91681635,0,0,0,0,0,0,0.40301943,0,0,0,3.2203531,0.2024988,0,0,0,0,0,0,1.2414606,0,0,0,1.2453537,0,4.912657,0,4.361135,0,0,0,0,0,0,0,2.4128122,0.68293965,0.7010477,1.3694913,1.7609701,0,0,0,0,0,0,0,])
-
+encoding_pred_slice=np.array([0,0,0,0,6.943786,0,0,0,0,0.495808,0,0,1.2099614,0,0,1.9550853,0,2.5830698,0,0,1.5520213,6.7467823,0.30691597,0.6208435,0,3.8465405,3.7862554,2.3970299,0,0,3.5254822,2.7294598,0,0,2.7226853,0,3.2338202,2.3976898,0,6.3592043,0,2.7090664,0,0,10.004378,0,0,0,3.0425727,2.0538316,1.8156273,0.15581878,2.3381875,0.88823074,0,0,0,0,0,0,0.036334604,0,0,0,3.5556676,0.29299664,0,0,0,0,0,0,1.0033253,0,0,0,0.96017045,0,5.8062425,0,4.4312,0,0,0,0,0,0,0,2.7901797,0.5715834,0.76234996,1.7294867,1.2244358,0,0,0,0,0,0,0,])
 
 def test_preprocess():
 	'''
@@ -53,5 +55,19 @@ def test_vgg16():
 	assert(x.shape==(1,224,224,3))
 	assert(preds.shape==(1,4096))
 	assert(len(preds[0][preds[0]!=0])==1368)
-	np.testing.assert_allclose(preds[0,:100],vgg16_pred_slice,err_msg='VGG16 embedding yielded wrong embedding for test image!')
+	np.testing.assert_allclose(preds[0,:100],vgg16_pred_slice,err_msg='VGG16 encoding yielded wrong encoding for test image!')
 
+def test_encodings():
+	'''
+	We test encode_image.encodings(), which is just a wrapper for VGG16.
+	This proceeds in the same fashion as in test_vgg16(). The truth value for
+	the encodings differ slightly, since the encodings() wrapper uses another
+	preprocessing routine defined in imagenet_utils.
+	'''
+	model=model_gen()
+	preds=encodings(model=model,path=os.path.join(image_folder,test_image))
+
+	assert(preds.shape==(4096,))
+	assert(len(preds[preds!=0])==1351)
+        np.testing.assert_allclose(preds[:100],encoding_pred_slice,err_msg='encoding wrapper yielded wrong encoding for test image!')
+	assert(True)
