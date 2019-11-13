231ce0acbd9a6ccacba232b6ad3cf5cbcef5f86e
Robert L. Schuhmann <robert.schuhmann.13@ucl.ac.uk>
2019-11-13 16:37:20 +0000

    working test for preprocessing

--- test_preprocess_encode.py
+++ test_preprocess_encode.py
@@ -9,14 +9,18 @@ captions_folder='Flickr8K_Text/'
 
 def test_preprocess():
 	'''
-	Wherein we test preprocess_data.preprocess(). We check whether the 42nd entries
-	of both output files, trainimgs.txt and testimgs.txt, agree with the truth.
+	Wherein we test preprocess_data.preprocess(). We check whether the number of 
+	test/training examples as well as the 42nd entries of both output files, 
+	trainimgs.txt and testimgs.txt, agree with the truth.
 	'''
 	preprocessing()
 
 	train_captions=open(os.path.join(captions_folder,'trainimgs.txt'),'r').read().split('\n')
 	test_captions=open(os.path.join(captions_folder,'testimgs.txt'),'r').read().split('\n')
 
-	print(train_captions)
+	assert len(train_captions)==30001#the last line yields one stray empty string
+	assert len(test_captions)==5001
 
+	assert train_captions[42]=='2638369467_8fc251595b.jpg\t<start> A smiling young girl in braids is playing ball . <end>'
+	assert test_captions[42]=='280706862_14c30d734a.jpg\t<start> A black dog on a beach carrying a ball in its mouth . <end>'
 	
