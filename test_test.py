ebe43c26ede4a9c5d9dba68a5474d62a44fb877c
Robert L. Schuhmann <robert.schuhmann.13@ucl.ac.uk>
2019-11-15 09:39:33 +0000

    adding new set of tests for frontend; adding unit test for caption post-processing

---
+++ test_test.py
@@ -0,0 +1,54 @@
+import pytest
+from tensorflow.python.client import device_lib
+import keras.backend as K
+
+from test_mod import process_caption,generate_captions
+from SceneDesc import scenedesc
+import encode_image as ei
+
+try:
+	sd=scenedesc()
+	model=sd.create_model(ret_model=True)
+	model.load_weights('Output/Weights.h5')
+	preload_model=True
+	print('pre-computed weights loaded; model created')
+
+except IOError:
+	print('no pre-computed weights found; skipping testing tests')
+	preload_model=False
+
+
+try:
+	tf_available_devices=str(device_lib.list_local_devices())
+	k_gpus=K.tensorflow_backend._get_available_gpus()	
+
+	if 'GPU' in tf_available_devices and len(k_gpus)>0:
+		print('GPUs visible to keras!')
+		has_gpu=True
+	else:
+		print('No GPUs visible to keras - skipping training tests!')
+		has_gpu=False
+		
+except AttributeError:
+	print('Could not detect whether any GPU is visible to keras - most likely due to an outdated version of keras. Skipping training tests!')
+	has_gpu=False
+
+
+def test_process_caption():
+	'''
+	Wherein we test the post-processing step in test_mod.py 
+	that removes start and end tokens.
+	'''
+	capt=process_caption(sd,'<start> A black dog is running after a white dog in the snow . <end>')
+	assert(capt=='A black dog is running after a white dog in the snow .')
+
+def test_generate_captions():
+	pass
+
+@pytest.mark.skipif(not preload_model, reason='no pre-computed weights found')
+def test_text_creation():
+	pass
+
+@pytest.mark.skipif(not has_gpu, reason='no gpu visible to keras')
+def test_training():
+	pass
