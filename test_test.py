9a2481918b5f6da657ce87047fc603aaa2969b6d
Robert L. Schuhmann <robert.schuhmann.13@ucl.ac.uk>
2019-11-15 11:14:57 +0000

    unit test for training procedure

--- test_test.py
+++ test_test.py
@@ -1,9 +1,11 @@
 import pytest
 from tensorflow.python.client import device_lib
 import keras.backend as K
+import os, subprocess, atexit
 
 from test_mod import process_caption,generate_captions
 from test import text
+from train import train
 from SceneDesc import scenedesc
 import encode_image as ei
 
@@ -56,7 +58,9 @@ def test_generate_captions():
 
 	encoded_img=ei.encodings(ei.model_gen(),test_img)
 	caption=generate_captions(sd,model,encoded_img,beam_size=3)
-	print('The model generated the caption: '+caption)
+	def report():
+		print('The model generated the caption: '+caption)
+	atexit.register(report)
 	assert(len(caption)>0)
 
 @pytest.mark.skipif(not preload_model, reason='no pre-computed weights found')
@@ -64,11 +68,68 @@ def test_text_creation():
 	'''
 	Wherein we test the caption generation wrapper test.text.
 	'''
-	print('\nThe text creation wrapper generated the caption: ')
 	text(test_img)
-	print('This should be the same as the caption generated above, and you should also have heard it read out.\n')
+	def report():
+		print('The model generated a caption that you should hear (and read, if you run pytest with the -s flag). This caption should be the same as the caption you can read below, and you should also have heard it read out.\n')
+	atexit.register(report)
 	pass
 
 @pytest.mark.skipif(not has_gpu, reason='no gpu visible to keras')
 def test_training():
-	pass
+	'''
+	Wherein we test whether we can run the training pipeline
+	for a single epoch. We afterwards check that the model and
+	weights files have been written to file and are nonzero.
+	If pre-computed model/weights files are present, they will
+	be copied to a temporary location to save them from being
+	wantonly and relentlessly overwritten.
+	'''
+
+	#First, we make sure that we do not overwrite our pre-computed model and weights
+
+	if os.path.exists(os.path.join('Output','Model.h5')):
+		try:
+			subprocess.call(['mv',os.path.join('Output','Model.h5'),os.path.join('Output','Model.h5.swp')])
+			swapped_model=True
+		except:
+			print('Found pre-computed model, but could not copy it in temporary location. Aborting test.')
+			proceed=False #model present, but copying failed
+			swapped_model=False
+	else:
+		proceed=True #there is no model to overwrite
+		swapped_model=False
+
+	if os.path.exists(os.path.join('Output','Weights.h5')):
+		try:
+			subprocess.call(['mv',os.path.join('Output','Weights.h5'),os.path.join('Output','Weights.h5.swp')])
+			swapped_weights=True
+		except:
+			print('Found pre-computed weights, but could not copy them in temporary location. Aborting test.')
+			proceed=False #weights present, but copying failed
+			swapped_weights=False
+	else:
+		proceed=True #there are no weights to overwrite
+		swapped_weights=False
+
+	#train for one epoch; check that new model and weight files exist and are nonempty
+	if proceed:
+		train(1)
+
+		assert(os.path.exists('Output','Model.h5'))
+		assert(os.path.exists('Output','Weights.h5'))
+		assert(os.path.getsize(os.path.join('Output','Model.h5'))>0)
+		assert(os.path.getsize(os.path.join('Output','Weights.h5'))>0)
+	
+	#swapping back model and weights from their temporary locations
+	if swapped_model:
+		try:
+			subprocess.call(['mv',os.path.join('Output','Model.h5.swp'),os.path.join('Output','Model.h5')])
+		except:
+			print('Could not copy back model file! Possibly it has been overwritten...')
+
+	if swapped_weights:
+		try:
+			subprocess.call(['mv',os.path.join('Output','Weights.h5.swp'),os.path.join('Output','Weights.h5')])
+		except:
+			print('Could not copy back weights file! Possibly it has been overwritten...')
+
