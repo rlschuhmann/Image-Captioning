8bf44beaf562b34cd296eee2cdef46237a02eb6a
Robert L. Schuhmann <robert.schuhmann.13@ucl.ac.uk>
2019-11-15 10:14:46 +0000

    unit tests for caption generation

--- test_test.py
+++ test_test.py
@@ -3,11 +3,15 @@ from tensorflow.python.client import device_lib
 import keras.backend as K
 
 from test_mod import process_caption,generate_captions
+from test import text
 from SceneDesc import scenedesc
 import encode_image as ei
 
+	
+test_img='Flickr8K_Data/997722733_0cb5439472.jpg'
+sd=scenedesc()
+
 try:
-	sd=scenedesc()
 	model=sd.create_model(ret_model=True)
 	model.load_weights('Output/Weights.h5')
 	preload_model=True
@@ -42,11 +46,27 @@ def test_process_caption():
 	capt=process_caption(sd,'<start> A black dog is running after a white dog in the snow . <end>')
 	assert(capt=='A black dog is running after a white dog in the snow .')
 
+@pytest.mark.skipif(not preload_model, reason='no pre-computed weights found')
 def test_generate_captions():
-	pass
+	'''
+	Wherein we test test_mod.generate_captions. Since you may
+	use pre-computed weights from any source only print the 
+	generated sentence to stdout and check that it is nonempty
+	'''
+
+	encoded_img=ei.encodings(ei.model_gen(),test_img)
+	caption=generate_captions(sd,model,encoded_img,beam_size=3)
+	print('The model generated the caption: '+caption)
+	assert(len(caption)>0)
 
 @pytest.mark.skipif(not preload_model, reason='no pre-computed weights found')
 def test_text_creation():
+	'''
+	Wherein we test the caption generation wrapper test.text.
+	'''
+	print('\nThe text creation wrapper generated the caption: ')
+	text(test_img)
+	print('This should be the same as the caption generated above, and you should also have heard it read out.\n')
 	pass
 
 @pytest.mark.skipif(not has_gpu, reason='no gpu visible to keras')
