2db0ae44bdcbc933c92085c9478c98f1274527af
Robert L. Schuhmann <robert.schuhmann.13@ucl.ac.uk>
2019-11-15 16:16:13 +0000

    fixing pyttsx version

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import encode_image as ei
 import SceneDesc
 import test_mod as tm
 import time as time
-import pyttsx
+import pyttsx3
 import sys
 def text(img):
 	t1= time.time()
@@ -15,8 +15,8 @@ def text(img):
 	encoded_images = ei.encodings(encode, image_path)
 
 	image_captions = tm.generate_captions(sd, model, encoded_images, beam_size=3)
-	engine = pyttsx.init()
-	print image_captions
+	engine = pyttsx3.init()
+	print(image_captions)
 	engine.say(	str(image_captions))
 	engine.runAndWait()
 
