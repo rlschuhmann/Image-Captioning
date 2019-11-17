f31a9e8eb7e9ecf9071f0df8c525bb4bb0318ee1
Robert L. Schuhmann <robert.schuhmann.13@ucl.ac.uk>
2019-11-17 22:26:08 +0000

    fixing typo

--- train.py
+++ train.py
@@ -14,11 +14,11 @@ def train(epoch,retrain=False,save=False):
     batch_size = 512
 
     if save:
-        callback=ModelCheckpoint(filepath=weightfile,verbose=1,save_best_only=True)
+        callback=[ModelCheckpoint(filepath=weightfile,verbose=1,save_best_only=True)]
     else:
         callback=None
 
-    model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=epoch, verbose=2, callbacks=[callback])
+    model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=epoch, verbose=2, callbacks=callback)
     model.save('Output/Model.h5', overwrite=True)
     model.save_weights('Output/Weights.h5',overwrite=True)
  
