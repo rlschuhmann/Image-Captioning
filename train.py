69beea494066f0e847825d29039c878f9e201eb3
Robert L. Schuhmann <robert.schuhmann.13@ucl.ac.uk>
2019-11-17 22:17:12 +0000

    training - adding options for reading in weights/saving

--- train.py
+++ train.py
@@ -1,12 +1,24 @@
 import SceneDesc
+from keras.callbacks import ModelCheckpoint
 
 import sys
 
-def train(epoch):
+def train(epoch,retrain=False,save=False):
     sd = SceneDesc.scenedesc()
-    model = sd.create_model()
+    weightfile='Output/Weights.h5'
+    if retrain:
+        model=sd.create_model(ret_model=True)
+        model.load_weights(weightfile)
+    else:
+        model = sd.create_model()
     batch_size = 512
-    model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=epoch, verbose=2, callbacks=None)
+
+    if save:
+        callback=ModelCheckpoint(filepath=weightfile,verbose=1,save_best_only=True)
+    else:
+        callback=None
+
+    model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=epoch, verbose=2, callbacks=[callback])
     model.save('Output/Model.h5', overwrite=True)
     model.save_weights('Output/Weights.h5',overwrite=True)
  
